c1768e1d0dcbd56ba704bdb1b0902f76e7c24dcc
jazmin <jsaraviaa98@gmail.com>
2023-09-08 21:30:39 -0300

    login/singup

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const path = require('path'); // Importa el módulo 'path'
 const mysql = require('mysql2');
+const session= require('express-session');
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -17,6 +18,19 @@ connection.connect((err) => {
     return;
   }
   console.log('Connected to the database');
+});
+
+// express-session
+app.use(session({
+  secret: 'secreto', 
+  resave: false,
+  saveUninitialized: true,
+}));
+
+// session disponible para todas las rutas
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
 });
 
 
@@ -103,21 +117,21 @@ app.post('/removeFromCart', (req, res) => {
 
 
 // Ruta para mostrar la página de métodos de pago
-app.post('/payment', (req, res) => {
-
-  const query = 'SELECT * FROM formapago';
-  
-  connection.query(query, (err, results) => {
-    if (err) {
-      console.error( err);
-    
-      return;
-    }
-    
-    
-    res.render('paymentMethods', { metodo: results });
-  });
-
+app.get('/payment', (req, res) => {
+  if (req.session.usuario) {
+    // Si el usuario tiene una sesión activa, procede a mostrar la página de métodos de pago
+    const query = 'SELECT * FROM formapago';
+    connection.query(query, (err, results) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      res.render('paymentMethods', { metodo: results });
+    });
+  } else {
+    // Si el usuario no tiene una sesión activa, redirige a la página de inicio de sesión
+    res.redirect('/login');
+  }
 });
 
 // Ruta para procesar el pago
@@ -230,28 +244,72 @@ app.get('/login', (req, res) => {
   
 });
 
+// Ruta para manejar la creacion de cuenta
+app.get('/createAccount', (req, res) => {
+  
+  res.render('createAccount'); 
 
-// Ruta para manejar el inicio de sesión
+
+});
+
+// En una ruta protegida que requiere autenticación
+app.get('/perfil', (req, res) => {
+  if (req.session.usuario) {
+    // El usuario tiene una sesión activa, muestra la página de perfil
+    // Accede a req.session.usuario para obtener la información del usuario
+  } else {
+    res.redirect('/login'); // Redirige al inicio de sesión si no hay sesión activa
+  }
+});
+
+
 app.post('/loginlog', (req, res) => {
   const nick = req.body.nick;
   const contrasenia = req.body.contrasenia;
 
-  // Verificar las credenciales en la base de datos
-  connection.query( 'SELECT * FROM usuario WHERE nick = ? AND contrasenia = ?',
-  [nick, contrasenia] ,(err, results) => {
-    if (err) {
-      console.error('Error al consultar la base de datos:', err);
-      res.redirect('/login');
-    } else if (results.length === 1) {
-      // Inicio de sesión exitoso   req.session.user = results[0];
-    
-      
-      res.redirect('/'); // Redirige a la página de inicio , { datos: results }
-    } else {
-      res.redirect('/login');
-    }
-  });
+  connection.query(
+    'SELECT * FROM usuario WHERE nick = ? AND contrasenia = ?',
+    [nick, contrasenia],
+    (err, results) => {
+      if (err) {
+        console.error('Error al consultar la base de datos:', err);
+        res.redirect('/login');
+      } else if (results.length === 1) {
+        // Inicio de sesión exitoso
+        const usuario = results[0];
 
+        // Almacena el nombre del usuario en la sesión
+        req.session.usuario = usuario.nick;
+
+        res.redirect('/'); // Redirige a la página de inicio
+      } else {
+        res.redirect('/login');
+      }
+    }
+  );
+});
+
+
+
+// Ruta para manejar el inicio de sesión
+app.post('/createAccount', (req, res) => {
+  const nick = req.body.nick;
+  const contrasenia = req.body.contrasenia;
+  const nombre = req.body.nombre;
+
+  // Verificar las credenciales en la base de datos
+  connection.query(
+    'INSERT INTO usuario (ABM, compras, contrasenia, nick) VALUES (?, ?, ?, ?)',
+    [null, null, contrasenia, nick],
+    (error, results) => {
+      if (error) {
+        console.error(error);
+       
+      }else{
+        res.redirect('/login');
+      }
+    }
+  );
 });
 
 
