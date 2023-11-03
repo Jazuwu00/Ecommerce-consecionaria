35354b65be5fe1073ca7b9495c324bc7ff6e1428
jazmin <jsaraviaa98@gmail.com>
2023-11-03 19:41:56 -0300

    new styles

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const bodyParser = require('body-parser');
 const app = express();
 const path = require('path'); // Importa el módulo 'path'
 const mysql = require('mysql2');
-const session= require('express-session');
+const session = require('express-session');
 const nodemailer = require('nodemailer');
 
 // express-session
 app.use(session({
   name: 'myCookie',
-  secret: 'secreto', 
+  secret: 'secreto',
   resave: false,
   saveUninitialized: true,
   resave: false,
   saveUninitialized: true,
   cookie: {
-    secure: false, 
-    maxAge: 3600000, 
+    secure: false,
+    maxAge: 3600000,
   }
 }));
 
@@ -38,8 +38,8 @@ app.use(express.static('public'));
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '', 
-  database: 'concesionarialpp', 
+  password: '',
+  database: 'concesionarialpp',
 });
 
 connection.connect((err) => {
@@ -50,34 +50,34 @@ connection.connect((err) => {
   console.log('Connected to the database');
 });
 
-const cart = []; 
+const cart = [];
 
 app.use((req, res, next) => {
-  res.locals.cartSize = cart.length; 
+  res.locals.cartSize = cart.length;
   next();
 });
 
 // Ruta para mostrar el contenido del carrito
 app.get('/cart', (req, res) => {
-    console.log(cart)
-      res.render('cart', { cart });
+  console.log(cart)
+  res.render('cart', { cart });
 });
-  
-    
+
+
 // ruta principal
 app.get('/', (req, res) => {
-      const query = 'SELECT * FROM autodisponible0km ';
-    
-      connection.query(query, (err, results) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).send('Error al obtener los autos');
-        }
-    
-      res.render('index', { autos: results } );
-      })
+  const query = 'SELECT * FROM autodisponible0km ';
+
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error al obtener los autos');
+    }
+
+    res.render('index', { autos: results });
+  })
 });
-  
+
 app.get('/addToCart/:id', (req, res) => {
   const idProducto = req.params.id;
 
@@ -99,7 +99,7 @@ app.get('/addToCart/:id', (req, res) => {
 
         res.redirect('/cart');
       } else {
-    
+
         connection.query(
           'SELECT * FROM accesorio WHERE codACC = ?',
           [idProducto],
@@ -108,7 +108,7 @@ app.get('/addToCart/:id', (req, res) => {
               res.status(500).send('Error al obtener el accesorio');
             } else if (accesorioResults.length > 0) {
               // Producto es un accesorio
-              
+
               const existingAccesorio = cart.find(item => item.id === idProducto);
 
               if (existingAccesorio) {
@@ -170,12 +170,12 @@ app.get('/addToCart/:id', (req, res) => {
   );
 });
 
-  
+
 // Ruta para eliminar productos del carrito
 app.get('/removeFromCart', (req, res) => {
-  
+
   const productId = req.query.productId;
-  
+
   const productIndex = cart.findIndex((item) => item.id === productId);
 
   if (productIndex !== -1) {
@@ -199,10 +199,10 @@ const transporter = nodemailer.createTransport({
 const sendMailAsync = util.promisify(transporter.sendMail).bind(transporter);
 
 app.get('/sendMail', async (req, res) => {
-  
+
   console.log(userName)
   try {
-   
+
     const cartTable = `
       <table style="text-align:center;  color: black; border-collapse: collapse; width: 70%;">
         <thead>
@@ -214,25 +214,25 @@ app.get('/sendMail', async (req, res) => {
         </thead>
         <tbody>
         ${cart.map(item => {
-          if (item.type === 'autonuevo') {
-            return `
+      if (item.type === 'autonuevo') {
+        return `
               <tr>
                 <td>${item.marca} ${item.modelo}</td>
                 <td>${item.quantity}</td>
                 <td>$${item.precio !== undefined ? item.precio : 0}</td>
               </tr>
             `;
-          } else if (item.type === 'accesorio') {
-            return `
+      } else if (item.type === 'accesorio') {
+        return `
               <tr>
                 <td>${item.nombre}</td>
                 <td>${item.quantity}</td>
                 <td>$${item.precio !== undefined ? item.precio : 0}</td>
               </tr>
             `;
-          } else if (item.type === 'reparacion') {
-        
-            return `
+      } else if (item.type === 'reparacion') {
+
+        return `
               <tr>
                 <td>${item.nombre}</td>
                 <td>${item.quantity}</td>
@@ -240,9 +240,9 @@ app.get('/sendMail', async (req, res) => {
                
               </tr>
             `;
-          } else if (item.type === 'autousado') {
-      
-            return `
+      } else if (item.type === 'autousado') {
+
+        return `
               <tr>
               <td>${item.marca} ${item.modelo}</td>
                 <td>${item.quantity}</td>
@@ -250,14 +250,14 @@ app.get('/sendMail', async (req, res) => {
                 
               </tr>
             `;
-          }
-        }).join('')}
+      }
+    }).join('')}
         </tbody>
       </table>
     `;
 
 
-    const total = cart.reduce((acc, item) => acc +( item.quantity * (item.precio !== undefined ? item.precio : 0)), 0);
+    const total = cart.reduce((acc, item) => acc + (item.quantity * (item.precio !== undefined ? item.precio : 0)), 0);
 
     const mailOptions = {
       from: 'jazsaraviav@gmail.com',
@@ -284,7 +284,7 @@ app.get('/sendMail', async (req, res) => {
     console.error('Error al enviar el correo electrónico:', error);
     res.status(500).send('Error al enviar el correo electrónico');
   }
-  
+
 });
 
 
@@ -306,64 +306,64 @@ app.post('/payment', (req, res) => {
   }
 });
 
-  // Ruta para mostrar los autos desde la base de datos
-  app.get('/autos', (req, res) => {
-    const itemsPerPage = 8;
-    const page = req.query.page || 1; 
-    const startIndex = (page - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const query = 'SELECT * FROM autodisponible0km LIMIT ?, ?';
-    connection.query(query, [startIndex, itemsPerPage], (err, results) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).send('Error al obtener los autos');
-      }
-      connection.query('SELECT COUNT(*) AS totalCount FROM autodisponible0km', (countErr, countResult) => {
-        if (countErr) {
-          console.error(countErr);
-          return res.status(500).send('Error al obtener el conteo de autos');
-        }
-        const autos = results;
-        const totalCount = countResult[0].totalCount;
-        const totalPages = Math.ceil(totalCount / itemsPerPage);
-  
-        res.render('autos', { autos: autos, totalPages: totalPages, currentPage: page });
-        
-      });
-    });
-  });
-  
-  // Ruta para reparaciones
-  app.get('/reparacion', (req, res) => {
-    const itemsPerPage = 8;
-    const page = req.query.page || 1; 
-    const startIndex = (page - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const query = 'SELECT * FROM reparaciondisponible LIMIT ?, ?';
-    connection.query(query, [startIndex, itemsPerPage], (err, results) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).send('Error al obtener los autos');
-      } 
-      connection.query('SELECT COUNT(*) AS totalCount FROM reparaciondisponible', (countErr, countResult) => {
-        if (countErr) {
-          console.error(countErr);
-          return res.status(500).send('Error al obtener el conteo de autos');
-        }
-  
-        const reparaciones = results;
-        const totalCount = countResult[0].totalCount;
-        const totalPages = Math.ceil(totalCount / itemsPerPage);
-  
-        res.render('reparacion', { reparaciones: reparaciones, totalPages: totalPages, currentPage: page });
-      });
-    });
-  });
-  
- // Ruta para autos usados
- app.get('/usados', (req, res) => {
+// Ruta para mostrar los autos desde la base de datos
+app.get('/autos', (req, res) => {
   const itemsPerPage = 8;
-  const page = req.query.page || 1; 
+  const page = req.query.page || 1;
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const query = 'SELECT * FROM autodisponible0km LIMIT ?, ?';
+  connection.query(query, [startIndex, itemsPerPage], (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error al obtener los autos');
+    }
+    connection.query('SELECT COUNT(*) AS totalCount FROM autodisponible0km', (countErr, countResult) => {
+      if (countErr) {
+        console.error(countErr);
+        return res.status(500).send('Error al obtener el conteo de autos');
+      }
+      const autos = results;
+      const totalCount = countResult[0].totalCount;
+      const totalPages = Math.ceil(totalCount / itemsPerPage);
+
+      res.render('autos', { autos: autos, totalPages: totalPages, currentPage: page });
+
+    });
+  });
+});
+
+// Ruta para reparaciones
+app.get('/reparacion', (req, res) => {
+  const itemsPerPage = 8;
+  const page = req.query.page || 1;
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const query = 'SELECT * FROM reparaciondisponible LIMIT ?, ?';
+  connection.query(query, [startIndex, itemsPerPage], (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error al obtener los autos');
+    }
+    connection.query('SELECT COUNT(*) AS totalCount FROM reparaciondisponible', (countErr, countResult) => {
+      if (countErr) {
+        console.error(countErr);
+        return res.status(500).send('Error al obtener el conteo de autos');
+      }
+
+      const reparaciones = results;
+      const totalCount = countResult[0].totalCount;
+      const totalPages = Math.ceil(totalCount / itemsPerPage);
+
+      res.render('reparacion', { reparaciones: reparaciones, totalPages: totalPages, currentPage: page });
+    });
+  });
+});
+
+// Ruta para autos usados
+app.get('/usados', (req, res) => {
+  const itemsPerPage = 8;
+  const page = req.query.page || 1;
   const startIndex = (page - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
   const query = 'SELECT * FROM autousado LIMIT ?, ?';
@@ -371,7 +371,7 @@ app.post('/payment', (req, res) => {
     if (err) {
       console.error(err);
       return res.status(500).send('Error al obtener los autos');
-    } 
+    }
     connection.query('SELECT COUNT(*) AS totalCount FROM autousado ', (countErr, countResult) => {
       if (countErr) {
         console.error(countErr);
@@ -394,7 +394,7 @@ app.get('/products', (req, res) => {
   connection.query(query, (err, results) => {
     if (err) {
       console.error(err);
-      
+
       return;
     }
     res.render('products', { products: results });
@@ -454,7 +454,7 @@ app.get('/autos/:id', (req, res) => {
               );
             } else {
               console.log('No se encontró el auto');
-            
+
               res.status(404).send('No se encontró el auto');
             }
           }
@@ -501,7 +501,7 @@ app.get('/product/:type/:id', (req, res) => {
         res.status(500).send('Error al obtener el producto');
       } else if (results.length > 0) {
         const productToShow = results[0];
-        res.render('product', { product: productToShow , type: tipoProducto  });
+        res.render('product', { product: productToShow, type: tipoProducto });
       } else {
         res.send(`No se encontró el producto con el ID ${idProducto}`);
       }
@@ -512,7 +512,7 @@ app.get('/product/:type/:id', (req, res) => {
 
 // Ruta  inicio de sesión
 app.get('/login', (req, res) => {
-  const message = req.query.message || ''; 
+  const message = req.query.message || '';
 
   res.render('login', { message });
 });
@@ -528,10 +528,10 @@ app.get('/createAccount', (req, res) => {
 // requiere autenticacion
 app.get('/perfil', (req, res) => {
   if (req.session.usuario) {
-   res.render('profile');
-   
+    res.render('profile');
+
   } else {
-    res.redirect('/login'); 
+    res.redirect('/login');
   }
 });
 
@@ -539,26 +539,26 @@ app.get('/perfil', (req, res) => {
 app.post('/loginlog', (req, res) => {
   const nick = req.body.nick;
   const contrasenia = req.body.contrasenia;
-  var  message = 'Contraseña o usuario incorrecto'
+  var message = 'Contraseña o usuario incorrecto'
   connection.query(
     'SELECT * FROM usuario WHERE nick = ? AND contrasenia = ?',
     [nick, contrasenia],
     (err, results) => {
       if (err) {
-       
+
         console.error('Error al consultar la base de datos:', err);
-        res.redirect('/login' );
+        res.redirect('/login');
       } else if (results.length === 1) {
-        
+
         const usuario = results[0];
 
         // Almacena el nombre del usuario en la sesión
         req.session.usuario = usuario.nick;
 
-        res.redirect('/'); 
+        res.redirect('/');
       } else {
-       
-        res.render('login',{ message });
+
+        res.render('login', { message });
       }
     }
   );
@@ -566,17 +566,17 @@ app.post('/loginlog', (req, res) => {
 
 // Ruta para cerrar la sesión
 app.get('/logout', (req, res) => {
-  
+
   req.session.destroy((err) => {
     if (err) {
       console.error('Error al cerrar la sesión:', err);
     }
 
-    res.clearCookie('myCookie'); 
+    res.clearCookie('myCookie');
     res.redirect('/login');
   });
 });
- 
+
 
 // Ruta creación de cuenta
 app.post('/createAccount', (req, res) => {
@@ -593,7 +593,7 @@ app.post('/createAccount', (req, res) => {
     (error, results) => {
       if (error) {
         console.error(error);
-      
+
       } else {
         if (results.length > 0) {
           console.log('ya existe cuenta con ese correo')
@@ -608,7 +608,7 @@ app.post('/createAccount', (req, res) => {
             (insertError, insertResults) => {
               if (insertError) {
                 console.error(insertError);
-             
+
               } else {
                 const successMessage = 'Cuenta creada con éxito. Inicia sesión ahora.';
                 res.redirect(`/login?message=${successMessage}`);
@@ -616,7 +616,7 @@ app.post('/createAccount', (req, res) => {
             }
           );
         }
-        
+
       }
     }
   );
